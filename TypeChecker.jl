3a8690a77bdc5c98cd5d451aa31cad04dcd59fe9
Leah Hanson <astrieanna@gmail.com>
2013-07-17 17:55:38 -0400

    added functions to run the checking function on all the methods of a generic function or all the generic functions in a module.

--- TypeChecker.jl
+++ TypeChecker.jl
@@ -14,3 +14,34 @@ function returnbasedonvalues(args...)
   return true # return is not concrete type; all args are concrete types
 end
 
+# make MethodTable iterable. I like foreach loops. :)
+Base.start(mt::MethodTable)           = mt.defs
+Base.next(mt::MethodTable, m::Method) = (m,m.next)
+Base.done(mt::MethodTable, m::Method) = false 
+Base.done(mt::MethodTable, i::())     = true
+
+# check all the methods of a generic function
+function check_function(f) #f should be a generic function
+  i = 0
+  for m in f.env
+    if returnbasedonvalues(f,m.sig)
+      println("$(f.env.name)$(m.sig) failed")
+    end
+    i += 1
+  end
+end
+
+# check all the generic functions in a module
+function check_all_module(m::Module)
+  for n in names(m)
+    try
+      f = eval(n) # I feel like this will fail
+      if isgeneric(f)
+        check_function(f)
+      end
+    catch e
+      println(e)
+    end
+  end
+end
+
