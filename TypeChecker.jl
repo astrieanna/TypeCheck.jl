c8b38bc1d1cc5b362d575c13c4a248d36802ff68
Leah Hanson <astrieanna@gmail.com>
2013-07-17 16:49:26 -0400

    refactored as suggested by Stefan

--- TypeChecker.jl
+++ TypeChecker.jl
@@ -1,21 +1,16 @@
 function returnbasedonvalues(args...)
   e = finfer(args...)
   body = e.args[3]
+  if isleaftype(body.typ) return false end
+
   argnames = e.args[1]
   argtuples = e.args[2][2]
-  argtypes = DataType[]
   for (sym,typ,x) in argtuples
-    if contains(argnames,sym)
-      push!(argtypes,typ)
-    end
-  end
-
-  if isleaftype(body.typ) return false end
-  for typ in argtypes
-    if !isleaftype(typ)
+    if contains(argnames,sym) && (!isleaftype(typ))
       return false
     end
   end
+
   return true # return is not concrete type; all args are concrete types
 end
 
