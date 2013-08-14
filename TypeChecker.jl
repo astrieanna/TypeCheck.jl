83a03dba105451314710d52dd5640f351462ab24
Leah Hanson <astrieanna@gmail.com>
2013-08-14 17:33:15 -0400

    made check_all_module work for modules other than base and improved error printing; bubbled up return type info to improve method failed messages.

--- TypeChecker.jl
+++ TypeChecker.jl
@@ -1,17 +1,18 @@
 function returnbasedonvalues(args...)
   e = code_typed(args...)[1] #why does this return an array? when would it be of size != 1?
   body = e.args[3]
-  if isleaftype(body.typ) return false end
+  if isleaftype(body.typ) return (body.typ,false) end
 
   argnames = map(x -> isa(x,Symbol) ? x : x.args[1],e.args[1])
   argtuples = e.args[2][2]
   for (sym,typ,x) in argtuples
     if contains(argnames,sym) && (!isleaftype(typ))
-      return false
+      return (body.typ,false)
     end
   end
 
-  return true # return is not concrete type; all args are concrete types
+  return (body.typ,true)
+  # return is not concrete type; all args are concrete types
   # what about functions that return an abstract type for other reasons? (bytestring)
   # what about functions that are just not type-inferred well enough?
   # if a function takes no arguments, should we return true or false?
@@ -21,8 +22,9 @@ end
 function check_function(f) #f should be a generic function
   i = 0
   for m in f.env
-    if returnbasedonvalues(f,m.sig)
-      println("$(f.env.name)$(m.sig) failed")
+    (typ,b) = returnbasedonvalues(f,m.sig)
+    if b
+      println("$(f.env.name)$(m.sig)::$typ failed")
     end
     i += 1
   end
@@ -32,12 +34,12 @@ end
 function check_all_module(m::Module)
   for n in names(m)
     try
-      f = eval(n)
+      f = eval(m,n)
       if isgeneric(f)
         check_function(f)
       end
     catch e
-      println(e)
+      println("$n: $e")
     end
   end
 end
