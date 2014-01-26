e129ef43cfea97084247dca3a621f28728dccc22
Leah Hanson <leah.a.hanson@gmail.com>
2014-01-26 11:57:34 -0600

    added tests to travis.

--- test/test.jl
+++ test/test.jl
@@ -1,19 +1,22 @@
-using TypeCheck, FactCheck
-
-facts("Check Return Types") do
-  for n in names(Base)
-    if isdefined(Base,n)
-      f = eval(Base,n)
-      if isgeneric(f) && typeof(f) == Function
-        context(string(n)) do
-          @fact TypeCheck._check_function(f) => anything #=> ([],0)
-          [@fact TypeCheck.istype(TypeCheck.returntype(e)) => true for e in code_typed(f)]
-          [@fact TypeCheck.returntype(e) => TypeCheck.istype for e in code_typed(f)]
-            
+module TestTypeCheck
+  using TypeCheck, FactCheck
+  
+  facts("Check Return Types") do
+    for n in names(Base)
+      if isdefined(Base,n)
+        f = eval(Base,n)
+        if isgeneric(f) && typeof(f) == Function
+          context(string(n)) do
+            @fact TypeCheck._check_function(f) => anything #=> ([],0)
+            [@fact TypeCheck.istype(TypeCheck.returntype(e)) => true for e in code_typed(f)]
+            [@fact TypeCheck.returntype(e) => TypeCheck.istype for e in code_typed(f)]
+          end
         end
+      else
+        @fact n => x->isdefined(Base,x)
       end
-    else
-      @fact n => x->isdefined(Base,x)
     end
   end
+
+  exitstatus()
 end
